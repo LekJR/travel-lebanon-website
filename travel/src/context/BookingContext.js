dd6c6781968ae917ed198a21e90c779cde9638c1
LekJR <97527028+LekJR@users.noreply.github.com>
2025-12-30 19:45:51 +0200

    Log API_BASE value in BookingContext

--- travel/src/context/BookingContext.js
+++ travel/src/context/BookingContext.js
@@ -6,6 +6,7 @@ import beirutmarathon from "../assets/Events/beirutmarathon.jpg";
 import tyresunset from "../assets/Events/tyresunset.jpg";
 import coastalride from "../assets/Events/coastalride.jpg";
 
+console.log("API_BASE =", API_BASE);
 const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:8080";
 const BookingContext = createContext();
 
